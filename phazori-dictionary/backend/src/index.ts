56a89e9f8df2365ff1cb60669a431ad042ebea72
Oscar Navarrete <oscar.navarrete.estevez@hotmail.com>
2024-07-13 13:18:12 +0200

    feat: added getter for pathori dictionary

--- phazori-dictionary/backend/src/index.ts
+++ phazori-dictionary/backend/src/index.ts
@@ -28,6 +28,36 @@ app.post('/api/name', async (req, res) => {
     }
 });
 
+// GET endpoint to retrieve an entry by the comun key
+app.get('/api/name/comun/:comun', async (req, res) => {
+    const { comun } = req.params;
+    try {
+        const name = await Name.findOne({ comun });
+        if (name) {
+            res.status(200).json(name);
+        } else {
+            res.status(404).json({ message: 'Name not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error });
+    }
+});
+
+// GET endpoint to retrieve an entry by the raiz_phazori key
+app.get('/api/name/raiz_phazori/:raiz_phazori', async (req, res) => {
+    const { raiz_phazori } = req.params;
+    try {
+        const name = await Name.findOne({ raiz_phazori });
+        if (name) {
+            res.status(200).json(name);
+        } else {
+            res.status(404).json({ message: 'Name not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error });
+    }
+});
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
